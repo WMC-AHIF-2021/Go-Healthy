4109bb73a44da8435d08f8feb27194d917bb6733
Lukas-Hoyer <lukas@hoyer.top>
2022-05-08 14:43:01 +0200

    add calculator

---
+++ src/calorieCalcuator/kcal-calculator.js
@@ -0,0 +1,69 @@
+function checksum() {
+    let inputSleep, inHoursSitting, inHoursSitLow, inHoursWalk, inHoursSitWalk, inHoursWork;
+    inputSleep = parseFloat(document.getElementById("inHoursSleep").value);
+    inputSleep = isNaN(inputSleep) ? 0 : inputSleep;
+    inHoursSitting = parseFloat(document.getElementById("inHoursSitting").value);
+    inHoursSitting = isNaN(inHoursSitting) ? 0 : inHoursSitting;
+    inHoursSitLow = parseFloat(document.getElementById("inHoursSitLow").value);
+    inHoursSitLow = isNaN(inHoursSitLow) ? 0 : inHoursSitLow;
+    inHoursSitWalk = parseFloat(document.getElementById("inHoursSitWalk").value);
+    inHoursSitWalk = isNaN(inHoursSitWalk) ? 0 : inHoursSitWalk;
+    inHoursWalk = parseFloat(document.getElementById("inHoursWalk").value);
+    inHoursWalk = isNaN(inHoursWalk) ? 0 : inHoursWalk;
+    inHoursWork = parseFloat(document.getElementById("inHoursWork").value);
+    inHoursWork = isNaN(inHoursWork) ? 0 : inHoursWork;
+    let summe = inputSleep + inHoursSitting + inHoursSitLow + inHoursWalk + inHoursSitWalk + inHoursWork;
+    if (summe != 24.0) {
+        document.getElementById("lbError").innerHTML = "Sum of hours per day must be 24! Current sum is " + summe + "!";
+        document.getElementById("lbError").style.color = "red";
+        return false;
+    }
+    else {
+        document.getElementById("lbError").innerHTML = "Sum of hours is 24 - all good!";
+        document.getElementById("lbError").style.color = "green";
+        return true;
+    }
+}
+function calculateEnergyConsumption() {
+    // Read Hour Activity
+    let inputSleep, inHoursSitting, inHoursSitLow, inHoursWalk, inHoursSitWalk, inHoursWork;
+    inputSleep = parseFloat(document.getElementById("inHoursSleep").value);
+    inputSleep = isNaN(inputSleep) ? 0 : inputSleep;
+    inHoursSitting = parseFloat(document.getElementById("inHoursSitting").value);
+    inHoursSitting = isNaN(inHoursSitting) ? 0 : inHoursSitting;
+    inHoursSitLow = parseFloat(document.getElementById("inHoursSitLow").value);
+    inHoursSitLow = isNaN(inHoursSitLow) ? 0 : inHoursSitLow;
+    inHoursSitWalk = parseFloat(document.getElementById("inHoursSitWalk").value);
+    inHoursSitWalk = isNaN(inHoursSitWalk) ? 0 : inHoursSitWalk;
+    inHoursWalk = parseFloat(document.getElementById("inHoursWalk").value);
+    inHoursWalk = isNaN(inHoursWalk) ? 0 : inHoursWalk;
+    inHoursWork = parseFloat(document.getElementById("inHoursWork").value);
+    inHoursWork = isNaN(inHoursWork) ? 0 : inHoursWork;
+    let summe = inputSleep + inHoursSitting + inHoursSitLow + inHoursWalk + inHoursSitWalk + inHoursWork;
+    let palFactor = (inputSleep * 0.95 + inHoursSitting * 1.2 + inHoursSitLow * 1.5 + inHoursSitWalk * 1.7 + inHoursWalk * 1.9 + inHoursWork * 2.4) / (summe);
+    let gender = document.getElementById("selGender").value;
+    let weight, height, energyConsumption = 0, age;
+    weight = parseFloat(document.getElementById("inWeight").value);
+    if (isNaN(weight) || (weight < 0)) {
+        alert("Weight must not be empty and a valid positive number!");
+        return;
+    }
+    height = parseFloat(document.getElementById("inHeight").value);
+    if (isNaN(height) || (height < 0)) {
+        alert("Height must not be empty and a valid positive number!");
+        return;
+    }
+    age = parseFloat(document.getElementById("inAge").value);
+    if (isNaN(age) || (age < 0)) {
+        alert("Age must not be empty and a valid positive number!");
+        return;
+    }
+    if (gender == "1") {
+        energyConsumption = 655.1 + 9.6 * weight + 1.8 * height - 4.7 * age;
+    }
+    else {
+        energyConsumption = 66.47 + 13.7 * weight + 5 * height - 6.8 * age;
+    }
+    document.getElementById("inEnergy").value = energyConsumption.toFixed().toString();
+}
+//# sourceMappingURL=kcal-calculator.js.map
