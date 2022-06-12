ce2d7d4b76e7d84056049298268ac8405be662cb
victoria149 <victoria.pradel@aon.at>
2022-06-12 18:15:07 +0200

    added loading screen

---
+++ build/loadingScreen.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    let loadingScreen = document.getElementById("loadingScreen");
+    loadingScreen.style.visibility = "hidden";
+    loadingScreen.style.opacity = "0";
+});
+//# sourceMappingURL=loadingScreen.js.map
---
+++ src/scripts/loadingScreen.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+    let loadingScreen = document.getElementById("loadingScreen");
+    loadingScreen.style.visibility = "hidden";
+    loadingScreen.style.opacity = "0";
+})
