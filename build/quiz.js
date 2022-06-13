4010b56816a7fdb1a74ed8334571ad1ea08a5ffc
michelle2709 <michellehartl2005.com>
2022-06-13 08:24:29 +0200

    quiz cleaner

--- build/quiz.js
+++ build/quiz.js
@@ -4,7 +4,6 @@ var tennis = 0;
 var golf = 0;
 function result() {
   var values = [];
-  //document.getElementById("uniqueID").value;
   values.push(document.forms["sport"]["favourite"].value);
   values.push(document.forms["sport"]["personality"].value);
   values.push(document.forms["sport"]["place"].value);
@@ -30,39 +29,4 @@ function result() {
   //window.location.href = "result.html";
   window.location = "result.html?form/val1="+soccer+"&form/val2="+basketball+"&form/val3="+tennis+"&form/val4="+golf;
 
-}
-
-function draw_graph(v1,v2,v3,v4) {
-  var element = document.getElementById("myChart");
-  element.width = "400";
-  element.height = "200";
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'polarArea',
-    data: {
-      datasets: [{
-        label: "Survey_Chart",
-        backgroundColor: ["#086375", "#1dd3b0","#affc41", "#b2ff9e"],
-        data: [
-          v1,v2,v3,v4
-        ]
-      },
-      ],
-      labels: [
-        'soccer',
-        'basketball',
-        'tennis',
-        'golf'
-      ]
-    },
-    options: {
-      title: {
-        display: false,
-        text: 'Chart'
-      },
-      events: [],
-      animation: false,
-    }
-  });
 }
